aa6fdcbf0741d1d9e448159b7498f2dcf99db703
Bui Trung Kien <buitrungkien2005qng@gmail.com>
2026-01-15 21:46:55 +0700

    complete ex4 part 2

--- js/script_ex_4.js
+++ js/script_ex_4.js
@@ -2,16 +2,27 @@ const CalcTip = (number) => {
   return number * (number >= 50 && number <= 300 ? 0.15 : 0.2);
 };
 
-var bills = [125, 555, 44];
+const calcAverage = (arr) => {
+  return (
+    arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0) /
+    arr.length
+  );
+};
 
-var tips = bills.map((b) => CalcTip(b));
+var bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+var tips = [];
 
 var total = [];
 
-for (let i = 0; i < bills.length; i++) {
-  total[i] = bills[i] + tips[i];
-}
+bills.forEach((val) => {
+  let tip = CalcTip(val);
+  tips.push(tip);
+  total.push(val + tip);
+});
 
 console.log(bills);
 console.log(tips);
 console.log(total);
+
+console.log(calcAverage(total));
