004ffb68ee6886665e01c57acb0f8e2de1795066
Bui Trung Kien <buitrungkien2005qng@gmail.com>
2026-01-15 20:26:46 +0700

    completed ex 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
 // Bai 1
 const BMI = (mass, height) => {
-    return mass / height ** 2
-}
+  return mass / height ** 2;
+};
 
 const btnCalc = document.getElementById("btn_calc");
 
@@ -16,23 +15,27 @@ const eleHeightJoin = document.getElementById("height_join");
 
 const value = document.getElementById("value");
 
+btnCalc.addEventListener("click", (e) => {
+  e.preventDefault();
 
-btnCalc.addEventListener('click', (e) => {
-    e.preventDefault();
+  let valMassMark = parseFloat(eleMassMark.value);
+  let valHeightMark = parseFloat(eleHeightMark.value);
+  let valMassJoin = parseFloat(eleMassJoin.value);
+  let valHeightJoin = parseFloat(eleHeightJoin.value);
 
-    let valMassMark = parseFloat(eleMassMark.value);
-    let valHeightMark = parseFloat(eleHeightMark.value);
-    let valMassJoin = parseFloat(eleMassJoin.value);
-    let valHeightJoin = parseFloat(eleHeightJoin.value);
+  let markBMI = BMI(valMassMark, valHeightMark);
+  let joinBMI = BMI(valMassJoin, valHeightJoin);
 
-    let markBMI = BMI(valMassMark, valHeightMark);
-    let joinBMI = BMI(valMassJoin, valHeightJoin);
-    console.log("BMI of Mark: " + markBMI);
-    console.log("BMI of Join: " + joinBMI);
-    let bool = markBMI > joinBMI;
-    console.log(bool ? "Mark BMI is higher than John" : "John's BMI is higher than Mark's!");
+  console.log("BMI of Mark: " + markBMI);
+  console.log("BMI of Join: " + joinBMI);
+
+  let bool = markBMI > joinBMI;
+
+  value.value = bool
+    ? `Mark BMI (${markBMI.toFixed(2)}) is higher than John (${joinBMI.toFixed(
+        2
+      )})`
+    : `John's BMI (${joinBMI.toFixed(
+        2
+      )}) is higher than Mark's (${markBMI.toFixed(2)})!`;
 });
-
-console.log(eleMassMark);
-
-
